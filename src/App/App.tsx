1659f49836b8cf6e6b8ebe405864dab11634a953
Marcos Danilo <marcosdanilo94@gmail.com>
2023-09-29 17:20:11 -0300

    feat: create the search component to get the input value and make the request.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,13 @@
 import Header from '../Header';
+import Search from '../Search';
 import './App.module.scss';
 
 function App() {
   return (
-    <Header title="Previsão do tempo" />
+    <>
+      <Header title="Previsão do tempo" />
+      <Search />
+    </>
   );
 }
 
---
+++ src/Search/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import style from './Search.module.scss';
+import getWeatherData from '../common/utils/requester';
+
+function Search() {
+  const [searchText, setSearchText] = useState('');
+
+  function searchWeather() {
+    getWeatherData(searchText);
+    setSearchText('');
+  }
+
+  return (  
+    <form className={style.forms}>
+      <label htmlFor='search-btn'>
+        <input
+          name='search-btn'
+          id='search-btn'
+          type="text"
+          placeholder="Insira aqui o nome da cidade"
+          value={searchText}
+          onChange={ (e) => setSearchText(e.target.value) }
+        />
+        <img
+          src='/images/searchIcon.png'
+          alt='search icon'
+          onClick={ () => searchWeather() }
+        />
+      </label>
+    </form>
+  )
+}
+
+export default Search;
