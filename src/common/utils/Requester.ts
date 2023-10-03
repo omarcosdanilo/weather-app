c0dfe6adbe2b4eca286462220a5af83b1d36e50a
Marcos Danilo <marcosdanilo94@gmail.com>
2023-10-03 16:09:38 -0300

    refactor: change the requester to an abstract class.
    create two methods to get the api data, the first to get the data about the city typed in the input and the second to get the data about the Brazil capitals.

---
+++ src/common/utils/Requester.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { ApiResponse } from "../../interfaces/IApiResponse";
+
+abstract class Requester {
+  static async getWeatherData(cityName: string) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=da923a70663b4e097f43a08ac7be4da2&lang=pt_br&units=metric`
+    try {
+      const response = await axios.get(url).then(response => response.data);
+      
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async getCapitalsData(capitals: string[]) {
+    try {
+      const response = await Promise.all(
+        capitals.map(capital => axios.get<ApiResponse>(
+          `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=da923a70663b4e097f43a08ac7be4da2&lang=pt_br&units=metric`
+        ).then(response => response.data))
+      );
+
+      return response;
+    } catch(error) {
+      console.log(error);
+    }
+  }
+}
+
+
+export default Requester;
